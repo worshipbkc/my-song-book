a67835a90a27bf7bd6ff10c7e68b1190a0e3c34f
worshipbkc <phallakhum6@gmail.com>
2026-09-17 14:23:13 +0700

    app.js

--- app.js
+++ app.js
@@ -1657,8 +1657,9 @@ function renderSongsListOnly() {
             const previewText = escapeHtml(song.lyrics).replace(/\[.*?\]/g, '').substring(0, 100) + '...';
             cardThumbnail = `
                 <div class="song-text-preview" onclick="openFullScreenModal('${song.id}')">
-                    <div class="text-preview-header"><i class="fa-solid fa-file-lines"></i> អត្ថបទចម្រៀង</div>
-                    <div style="white-space: pre-wrap;">${previewText}</div>
+                    <!-- ប្តូរពីពាក្យអត្ថបទចម្រៀង ទៅជាចំណងជើងបទ (${escapeHtml(song.title)}) វិញ -->
+                    <div class="text-preview-header" style="font-size: 1rem;"><i class="fa-solid fa-music"></i> ${escapeHtml(song.title)}</div>
+                    <div style="white-space: pre-wrap; font-family: 'Kantumruy Pro', sans-serif; color: var(--text); line-height: 1.5; margin-top: 4px;">${previewText}</div>
                 </div>`;
         } else {
             cardThumbnail = `<img src="https://via.placeholder.com/300x400?text=No+Data" class="song-img" onclick="openFullScreenModal('${song.id}')">`;
